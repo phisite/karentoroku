ca483a842a5dcf89110d4acc59af3a61634852e6
Phisit Siprasatthong <phisite@gmail.com>
2025-06-30 17:02:04 +0700

    feat: Move Firebase config to .env
    Moved Firebase credentials from src/components/auth/config.jsx to a .env file to prevent leaking secrets.
    
    Updated src/components/auth/config.jsx to read the credentials from environment variables.
    
    Added .env to .gitignore to prevent the credentials from being committed to the repository.

--- src/components/auth/config.jsx
+++ src/components/auth/config.jsx
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyAc0ujDQ1_CvZURQFAXi87GFvxP0rt6VVA",
-  authDomain: "karentoroku.firebaseapp.com",
-  projectId: "karentoroku",
-  storageBucket: "karentoroku.appspot.com",
-  messagingSenderId: "265114371179",
-  appId: "1:265114371179:web:20c12dcd2009be49045d2e",
-  measurementId: "G-DKG3JGNXTW",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
